84eab3ec3d5a05e7ffc8ff1cd3b287b736873b92
LudmilaMiguens <miguensludmila2@gamil.com>
2023-09-01 21:12:33 -0300

    Agregue y correji unos metodos de la clase Biblioteca

--- Clase 8/biblioteca.ts
+++ Clase 8/biblioteca.ts
@@ -97,23 +97,52 @@ class Biblioteca{
     public getDomicilio(): string{
         return this.domicilio;
     }
-    public getCliente(cliente: Cliente): void {
+    public addCliente(cliente: Cliente): void {
         this.clientes.push(cliente);
     }
-    public getElemento(elemento: Libreria): void{
+    public listaClientes(): void{
+        console.log(this.clientes);
+    }
+    public addElemento(elemento: Libreria): void{
         this.elementos.push(elemento);
     }
-    public getPrestamos(cliente:Cliente, elemento:Libreria): void{
+    public listaElementos(): Libreria[]{
+        return this.elementos
+    }
+    public addPrestamos(cliente:Cliente, elemento:Libreria): void{
         const nuevoPrestamo = new Prestamos(cliente,elemento);
         this.prestamos.push(nuevoPrestamo);
         console.log(this.prestamos);
-    
     }   
+
+    public listadoPrestamos(): Prestamos[]{
+        return this.prestamos;
+    }
+
 }
 let biblioteca1 = new Biblioteca("AS", "25 de mayo");
 let libro1 = new Libro("Casita", "Cosito", 1896,);
+let libro2 = new Libro("Casitassss", "Cositosss", 18966,);
+let revista1 = new Revista("Moda", "Pindonga", 2013);
 let cliente1 = new Cliente("Ludmila", "Miguens", "uriburu", 289355);
-let cliente2 =new Cliente ("Toque", "Ruiz","pepito 22", 25892)
+let cliente2 =new Cliente ("Toque", "Ruiz","pepito 22", 25892);
+let cliente3 = new Cliente ("pepita", "Lopez", "Peron 55", 558542);
 let prestamo1 = new Prestamos(cliente1, libro1);
 let prestamo2 = new Prestamos (cliente2, libro1);
-biblioteca1.getPrestamos(cliente1,libro1);
+let prestamo3 = new Prestamos (cliente3, revista1);
+
+biblioteca1.addPrestamos(cliente1,libro1);
+biblioteca1.addPrestamos(cliente2,libro2);
+biblioteca1.addPrestamos(cliente3,revista1);
+
+biblioteca1.addCliente(cliente1);
+biblioteca1.addCliente(cliente2);
+biblioteca1.addCliente(cliente3);
+
+biblioteca1.addElemento(libro1);
+biblioteca1.addElemento(libro2);
+biblioteca1.addElemento(revista1);
+
+biblioteca1.listaClientes();
+console.log(biblioteca1.listaElementos());
+console.log(biblioteca1.listadoPrestamos());
